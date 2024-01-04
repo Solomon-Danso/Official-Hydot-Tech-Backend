831228c565048e8731254d709d6c1b3bd7cdf1bf
Solomon <solomondanso2023@gmail.com>
2024-01-04 14:03:24 +0000

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->longText('name');
-            $table->longText('email')->unique();
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->longText('password');
             $table->rememberToken();
